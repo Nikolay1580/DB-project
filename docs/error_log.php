98a5d72667087c78c37eeb59cdfb7a5ad71533b9
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2024-11-06 14:34:32 +0100

    meow

--- docs/error_log.php
+++ docs/error_log.php
@@ -26,12 +26,22 @@ function logError($errorMessage)
     }
 }
 
-// Example usage of error logging
-// Simulating an error for demonstration purposes
-try {
-    if (!file_exists('some_nonexistent_file.txt')) {
-        throw new Exception("File does not exist: some_nonexistent_file.txt");
+set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+    $errorMessage = "Error [$errno]: $errstr in $errfile on line $errline";
+    logError($errorMessage);
+    return true;
+});
+
+set_exception_handler(function ($exception) {
+    $errorMessage = "Uncaught exception: " . $exception->getMessage();
+    logError($errorMessage);
+});
+
+
+register_shutdown_function(function () {
+    $error = error_get_last();
+    if ($error !== null) {
+        $errorMessage = "Fatal error: {$error['message']} in {$error['file']} on line {$error['line']}";
+        logError($errorMessage);
     }
-} catch (Exception $e) {
-    logError($e->getMessage());
-}
+});
