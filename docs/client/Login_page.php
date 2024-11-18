83a4fae451d76aa389f9e91ec7de13e362579e27
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2024-11-18 16:35:04 +0100

    I hope this works

--- docs/client/Login_page.php
+++ docs/client/Login_page.php
@@ -1,5 +1,21 @@
 <?php
-include './access_log.php';
+require __DIR__ . "../server/gspot_lib.php";
+
+loadDotEnv(__DIR__ . "../server/.env");
+$apiKey = getenv("API_Key");
+
+
+$clientIp = getUserIP();
+
+$geoUrl = "https://ipinfo.io/{$clientIp}/json?token={$apiKey}";
+
+// Fetch geolocation data
+$response = @file_get_contents($geoUrl);
+$geoData = $response ? json_decode($response, true) : ['loc' => '0,0', 'ip' => $clientIp];
+
+$location = $geoData['loc'];
+$latitude = explode(',', $location)[0];
+$longitude = explode(',', $location)[1];
 ?>
 <!DOCTYPE html>
 <html lang="en">
