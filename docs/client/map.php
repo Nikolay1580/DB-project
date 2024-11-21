dfa5320427febe127e48b26569d32567531eb71b
Mateo-Pico <mateo.pico2004@gmail.com>
2024-11-21 18:50:25 +0100

    Added a CSS for the map page

--- docs/client/map.php
+++ docs/client/map.php
@@ -19,6 +19,7 @@ $clientIp = $geoData['ip'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IP Geolocation Map</title>
+    <link rel="stylesheet" href="map.css">
     <!-- Leaflet CSS -->
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
